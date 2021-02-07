e10b96e9f24c334889b708c42452d5b55a5c79ff
Robert Campagna <rwcnyla@gmail.com>
2021-02-07 11:20:47 -0800

    finished mainhandlebars

--- public/javascript/add-recipe.js
+++ public/javascript/add-recipe.js
@@ -2,7 +2,7 @@ async function newFormHandler(event) {
     event.preventDefault();
   
     const title = document.querySelector('input[name="post-recipe"]').value;
-    const post_url = document.querySelector('input[name="post-url"]').value;
+    // const post_Recipe = document.querySelector('input[name="post-url"]').value;
   
     const response = await fetch(`/api/posts`, {
       method: 'POST',

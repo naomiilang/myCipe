ff76712bccf53def45ef6418279ff4ed254b5ed8
Robert Campagna <rwcnyla@gmail.com>
2021-02-09 20:15:35 -0800

    linked signup and login css and javascsript

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,5 +6,17 @@ const { Recipe, User, Favorite, Comment } = require('../models');
 router.get('/', (req, res) => {
     res.render('homepage');
   });
-  
+router.get('/login', (req, res) => {
+    res.render('login');
+  });
+router.get('/signup', (req, res) => {
+    res.render('signup');
+  });
+// router.get('/', (req, res) => {
+//     res.render('homepage');
+//   });
+
+//   router.get('/login', (req, res) =>{
+
+//   })
 module.exports = router;

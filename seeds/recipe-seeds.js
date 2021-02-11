7557158a45cb647d8dbedf24049dc5b48434c345
Erica Siegel <siegel.erica@gmail.com>
2021-02-11 14:50:27 -0800

    added single recipe

--- seeds/recipe-seeds.js
+++ seeds/recipe-seeds.js
@@ -3,15 +3,17 @@ const { Recipe } = require('../models');
 const recipeData = [
     {
         title: 'Making Spaghetti',
-        recipe_text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        recipe_text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         ingredients_text: 'tomato, salt, pepper, parmesean',
-        directions_text: 'boil pasta, make sauce, eat!'
+        directions_text: 'boil pasta, make sauce, eat!',
+        user_id: 1
     },
     {
         title: 'Baking Cake',
-        recipe_text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        recipe_text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         ingredients_text: 'cake mix, oil, egg',
-        directions_text: 'mix ingredients, bake, eat!'
+        directions_text: 'mix ingredients, bake, eat!',
+        user_id: 1
     }
 ];
 

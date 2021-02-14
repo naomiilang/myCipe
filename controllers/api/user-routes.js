f2ac9861e1b4c759039e948832a4427ef8f76873
nrhuizar <nrhuizar@gmail.com>
2021-02-14 12:30:44 -0800

    checking ../../models

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Recipe, Favorite, Comment } = require('../../models');
+const { User, Recipe, Favorite, Comment } = require('../../models.js');
 
 // GET /api/users
 router.get('/', (req, res) => {
